d8a722beef0f2f068fc4851bbdf78e9265307d2b
Ben Smith <bens15351@gmail.com>
2023-02-03 13:34:56 +0000

    Added a queue for the ADC samples

--- Drivebase/Motor Speed/Motor Speed.ino
+++ Drivebase/Motor Speed/Motor Speed.ino
@@ -1,7 +1,7 @@
 /* 
 TODO:
 - Create rolling average of motor frequencies (weighted?)
-- Add PID control loop
+- Finish PID control loop
 - Add usbHandle()
 - Add IMU over I2C and process data
 */
@@ -10,6 +10,8 @@ TODO:
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <queue.h>
+#include <semphr.h>
 
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
@@ -46,26 +48,33 @@ TODO:
 
 //-------------------------------- Global Variables -------------------------------------
 
-const uint32_t pwmFrequency = 20000;                  // Sets the PWM frequency (in Hz)
-const uint8_t pwmResolution = 16;                     // Sets the PWM resolution (in bits), default is 16
-const uint16_t pwmRange = pow(2, pwmResolution) - 1;  // Calculates the PWM range based on the resolution
-const uint8_t adcResolution = 10;                     // Sets the ADC resolution
-const uint16_t adcRange = pow(2, adcResolution) - 1;  // Calculates the ADC range
+static const uint32_t pwmFrequency = 20000;                  // Sets the PWM frequency (in Hz)
+static const uint8_t pwmResolution = 16;                     // Sets the PWM resolution (in bits), default is 16
+static const uint16_t pwmRange = pow(2, pwmResolution) - 1;  // Calculates the PWM range based on the resolution
+static const uint8_t adcResolution = 10;                     // Sets the ADC resolution
+static const uint16_t adcRange = pow(2, adcResolution) - 1;  // Calculates the ADC range
 
 
-const uint16_t samples = 64;                                    // Number of samples collected before processing
-const uint16_t samplingFrequency = 1000;                        // Sampling frequency in Hz, default: 1000
-const uint8_t numberOfMotors = 3;                               // Number of motors, default: 3
-const int8_t motors[numberOfMotors] = { HALL1, HALL2, HALL3 };  // Vector to store the pins to sample for each motor
-const float thresholdStandardDeviation = 30;                    // Threshold in standard deviation to distinguish between noise and a signal
+static const uint16_t samples = 64;                                    // Number of samples collected before processing
+static const uint16_t samplingFrequency = 1000;                        // Sampling frequency in Hz, default: 1000
+static const uint8_t numberOfMotors = 3;                               // Number of motors, default: 3
+static const int8_t motors[numberOfMotors] = { HALL1, HALL2, HALL3 };  // Vector to store the pins to sample for each motor
+static const float thresholdStandardDeviation = 30;                    // Threshold in standard deviation to distinguish between noise and a signal
 
-volatile uint16_t sampleNumber = 0;          // Variable to store current sample number
-volatile uint16_t motorSamples[3][samples];  // Vector to store ADC samples from the motor 1 hall effect sensor
+static volatile uint16_t sampleNumber = 0;          // Variable to store current sample number
+static volatile uint16_t motorSamples[3][samples];  // Vector to store ADC samples from the motor 1 hall effect sensor
 
-volatile double targetMotorFrequency[3] = { 0, 0, 0 };  // Vector to store the target motor frequencies (in Hz)
-volatile double actualMotorFrequency[3] = { 0, 0, 0 };  // Vector to store the motor freqiencies (in Hz)
+static volatile uint16_t controlFrequency = 10;                // Control loop frequenct in Hz, default: 10
+static volatile double targetMotorFrequency[3] = { 0, 0, 0 };  // Vector to store the target motor frequencies (in Hz)
+static volatile double actualMotorFrequency[3] = { 0, 0, 0 };  // Vector to store the motor freqiencies (in Hz)
 
-volatile bool samplesReady = false;  // Variable true when the array of ADC samples is full, false when
+static volatile bool samplesTransferred = false;  // Variable true when ADC data has been transferred to the processing task
+
+// Sample queue
+static QueueHandle_t sampleQueue;
+
+// Data ready semaphore
+//static SemaphoreHandle_t dataReadyBinSem;
 
 //-------------------------------- Functions --------------------------------------------
 
@@ -90,7 +99,7 @@ float standardDeviation(uint16_t population[], uint16_t populationSize, uint16_t
   return populationStandardDeviation;
 }
 
-// Gets the frequency of a sinusoidal/sqare wave input as an array using its sampling frequency, average value and standard deviation
+// Gets the frequency of a sinusoidal wave input as an array using its sampling frequency, average value and standard deviation
 float getFrequency(uint16_t signal[], uint16_t sampleNumber, uint16_t mean, uint16_t samplingFrequency, float standardDeviation) {
 
   // Define local variables
@@ -120,112 +129,7 @@ float getFrequency(uint16_t signal[], uint16_t sampleNumber, uint16_t mean, uint
   return frequency;
 }
 
-//-------------------------------- Task Functions ----------------------------------------
-
-// Task function to sample hall sensors and add the samples to an array
-void readHallSensorsTask(void* pvParameters) {
-
-  (void)pvParameters;
-
-  // Set input pins
-  pinMode(HALL1, INPUT);
-  pinMode(HALL2, INPUT);
-  pinMode(HALL3, INPUT);
-
-  // Setup timer so this task executes at the frequency specified in samplingFrequency
-  const TickType_t xFrequency = configTICK_RATE_HZ / samplingFrequency;
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-
-  // Start the loop
-  while (true) {
-
-    // Pause the task until enough time has passed
-    vTaskDelayUntil(&xLastWakeTime, xFrequency);
-
-    // Sample each hall effect sensor and store in motorSamples
-    for (uint8_t motor = 0; motor < numberOfMotors; motor++) {
-      motorSamples[motor][sampleNumber] = analogRead(motors[motor]);
-    }
-
-    // Increment the sample number, and if the array is full then tell the processing function the samples are ready
-    if (++sampleNumber > samples - 1) {
-      samplesReady = true;
-
-      // Wait until the samples have been read into the processing function
-      while (samplesReady) {
-        vTaskDelay(pdMS_TO_TICKS(1));
-      }
-
-      // Resest the sample array
-      sampleNumber = 0;
-    }
-  }
-}
-
-// Task function to calculate the motor frequencies
-void getMotorFrequencyTask(void* pvParameters) {
-
-  (void)pvParameters;
-
-  // Create local variables
-  uint16_t data[numberOfMotors][samples];  // Non-volatile array to store the sample data
-  float standardDeviations[3];             // Array to store standard deviations
-
-  // Start the loop
-  while (true) {
-
-    // If sample array is full: process the data
-    if (samplesReady) {
-
-      // Create a non volatile 2D array so the samples can be processed without the data changing
-      for (uint8_t motor = 0; motor < numberOfMotors; motor++) {
-        for (uint16_t sampleNumber = 0; sampleNumber < samples; sampleNumber++) {
-          data[motor][sampleNumber] = motorSamples[motor][sampleNumber];
-        }
-      }
-
-      // Once the data has been copied, tell the readHallSensorsTask to resume
-      samplesReady = false;
-
-      // Loop through each motor and find frequencies
-      for (uint8_t motor = 0; motor < numberOfMotors; motor++) {
-        
-        // Calculate standard deviation
-        standardDeviations[motor] = standardDeviation(data[motor], samples, (pwmRange + 1) / 2);
-
-        // If standard deviation greater than threshold value then a signal is present
-        if (standardDeviations[motor] > thresholdStandardDeviation) {
-
-          // If signal present, work out its frequency
-          actualMotorFrequency[motor] = getFrequency(data[motor], samples, (adcRange + 1) / 2, samplingFrequency, standardDeviations[motor]);
-        }
-
-        // Else it is just noise
-        else {
-          actualMotorFrequency[motor] = 0;
-        }
-
-        // Check every millisecond if there is data to be processed
-        vTaskDelay(pdMS_TO_TICKS(1));
-      }
-    }
-  }
-}
-
-// Task function to run the pid control loop
-void pidControllerTask(void* pvParameters) {
-
-  (void)pvParameters;
-
-  while (true) {
-    vTaskDelay(pdMS_TO_TICKS(500));
-    digitalWrite(LED_BUILTIN, HIGH);
-    vTaskDelay(pdMS_TO_TICKS(500));
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-}
-
-// Function to print the current status of each task
+// Function to print the current status of each task, used for debugging
 std::map<eTaskState, const char*> eTaskStateName{ { eReady, "Ready" }, { eRunning, "Running" }, { eBlocked, "Blocked" }, { eSuspended, "Suspended" }, { eDeleted, "Deleted" } };
 void ps() {
   int tasks = uxTaskGetNumberOfTasks();
@@ -240,20 +144,143 @@ void ps() {
   delete[] pxTaskStatusArray;
 }
 
+//-------------------------------- Task Functions ----------------------------------------
+
+// Task function to sample hall sensors and add the samples to an array
+void readHallSensorsTask(void* pvParameters) {
+
+  (void)pvParameters;
+
+  // Set input pins
+  pinMode(HALL1, INPUT);
+  pinMode(HALL2, INPUT);
+  pinMode(HALL3, INPUT);
+
+  // Create local variables
+  uint16_t currentSample;
+
+  // Setup timer so this task executes at the frequency specified in samplingFrequency
+  const TickType_t xFrequency = configTICK_RATE_HZ / samplingFrequency;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  // Start the loop
+  while (true) {
+
+    // Pause the task until enough time has passed
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+    // Sample each hall effect sensor and store in motorSamples
+    for (uint8_t motor = 0; motor < numberOfMotors; motor++) {
+//      motorSamples[motor][sampleNumber] = analogRead(motors[motor]);
+      currentSample = analogRead(motors[motor]);
+      xQueueSend(sampleQueue, (void *)&currentSample , (TickType_t)0);
+    }
+
+    // Increment the sample number, and if the array is full then tell the processing function the samples are ready
+    if (++sampleNumber > samples - 1) {
+
+      // Wait until the samples have been read into the processing function
+      while (!samplesTransferred) {
+        vTaskDelay(pdMS_TO_TICKS(1));
+      }
+
+      // Resest variables before the next loop
+      sampleNumber = 0;
+      samplesTransferred = false;
+    }
+  }
+}
+
+// Task function to calculate the motor frequencies
+void getMotorFrequencyTask(void* pvParameters) {
+
+  (void)pvParameters;
+
+  // Create local variables
+  uint16_t data[numberOfMotors][samples];  // Non-volatile array to store the sample data
+  float standardDeviations[numberOfMotors];             // Array to store standard deviations
+
+  // Start the loop
+  while (true) {
+    Serial.print(actualMotorFrequency[0]);    
+
+    // Read the samples from the sample queue into a non volatile 2D array so they can be processed without the data changing
+    for (uint8_t motor = 0; motor < numberOfMotors; motor++) {
+      for (uint16_t sampleNumber = 0; sampleNumber < samples; sampleNumber++) {
+        xQueueReceive(sampleQueue, (void *)&data[motor][sampleNumber], portMAX_DELAY);
+      }
+    }
+
+    // Once the data has been copied, tell the readHallSensorsTask to resume
+    samplesTransferred = true;
+
+    // Loop through each motor and find frequencies
+    for (uint8_t motor = 0; motor < numberOfMotors; motor++) {
+
+      // Calculate standard deviation
+      standardDeviations[motor] = standardDeviation(data[motor], samples, (pwmRange + 1) / 2);
+
+      // If standard deviation greater than threshold value then a signal is present
+      if (standardDeviations[motor] > thresholdStandardDeviation) {
+
+        // If signal present, work out its frequency
+        actualMotorFrequency[motor] = getFrequency(data[motor], samples, (adcRange + 1) / 2, samplingFrequency, standardDeviations[motor]);
+      }
+
+      // Else it is just noise
+      else {
+        actualMotorFrequency[motor] = 0;
+      }
+    }
+  }
+}
+
+// Task function to run the pid control loop
+void pidControllerTask(void* pvParameters) {
+
+  (void)pvParameters;
+
+  // Create local variables
+  float previousError[numberOfMotors] = { 0, 0, 0 };
+  float currentError[numberOfMotors] = { 0, 0, 0 };
+  float totalError[numberOfMotors] = { 0, 0, 0 };
+  float changeInError[numberOfMotors] = { 0, 0, 0 };
+
+  // Make the task execute at a specified frequency
+  const TickType_t xFrequency = configTICK_RATE_HZ / controlFrequency;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  // Start the loop
+  while (true) {
+
+    // Pause the task until enough time has passed
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+    // Loops through each motor
+    for (uint8_t motor = 0; motor < numberOfMotors; motor++) {
+
+      currentError[motor] = targetMotorFrequency[motor] - actualMotorFrequency[motor];
+      totalError[motor] += currentError[motor];
+      changeInError[motor] = (currentError[motor] - previousError[motor]) * controlFrequency;
+    }
+  }
+}
+
+
 //-------------------------------- Setups -----------------------------------------------
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  // put your setup code here, to run once:
+
+  // Setup USB
   Serial.begin(115200);
-}
 
-void setup1() {
-
+  // Define analogue parameters
   analogWriteFreq(pwmFrequency);
   analogWriteResolution(pwmResolution);
-
   analogReadResolution(adcResolution);
+
+  // Sets IO pins
+  pinMode(LED_BUILTIN, OUTPUT);
 
   // Create task handles
   TaskHandle_t readHallSensorsHandle = nullptr;
@@ -288,19 +315,18 @@ void setup1() {
   vTaskCoreAffinitySet(getMotorFrequencyHandle, (UBaseType_t)0x03);
   vTaskCoreAffinitySet(pidControllerHandle, (UBaseType_t)0x03);
 
+  // Create queues
+  sampleQueue = xQueueCreate(samples * numberOfMotors, sizeof(uint16_t));  // Queue length is the number of samples, item size is 2 bytes (uint16_t)
 
-  delay(5000);
+  // Create semaphores
+//  dataReadyBinSem = xSemaphoreCreateBinary();
+
+  // Delete "setup and loop" task
+  vTaskDelete(NULL);
 }
 
-//-------------------------------- Loops ------------------------------------------------
+//-------------------------------- Loop ------------------------------------------------
 
 void loop() {
-
-  ps();
-
-  vTaskDelay(pdMS_TO_TICKS(1000));
-}
-
-void loop1() {
-  vTaskDelay(pdMS_TO_TICKS(1000));
+  // Should never get to this point
 }
